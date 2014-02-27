bca3a5d3805a586bb9dd26dbaf11e742e272b0a3
Alex Wolczek <citoyen89@gmail.com>
2014-02-27 10:31:24 +0200

    Initial version

---
+++ hw1/js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("#searchSubmit").click(function(){
+        $("#homepage").addClass("hidden");
+        $("#searchresults").removeClass("hidden");
+
+        //$("#homepage").hide();
+        //$("#homepage").hidden = true;
+    });
+});
